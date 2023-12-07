6aaf93d83d4f099acc739c020d14269b9d5c33af
FernandoLorca <florcapinto@gmail.com>
2023-12-07 15:58:22 -0300

    Git test

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,6 @@ const main = async () => {
   try {
     // await sequelize.sync({ force: true });
     await sequelize.sync();
-    console.log('Prueba de conexión exitosa');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.error(`Unable to connect to the database: ${error}`);
